09284d6dafaf979e48e34123bac44598f3ef2535
maretha <marethautami3@gmail.com>
2015-10-22 19:58:05 +0700

    tugas3

---
+++ src/paket2/lingkaran.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paket2;
+
+/**
+ *
+ * @author Novriani
+ */
+public class lingkaran {
+    public double hasil;
+    public double lingkaran (int jari){
+        hasil = 3.14*jari*jari;
+        return hasil;
+    }
+    
+}
---
+++ src/paket2/persegi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paket2;
+
+/**
+ *
+ * @author Novriani
+ */
+public class persegi {
+    public int hasil;
+    public int persegi (int sisi1, int sisi2){
+        hasil = sisi1 * sisi2;
+        return hasil;
+    }
+    
+}
